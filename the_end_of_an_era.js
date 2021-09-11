a4efb7a523ca8949c616a9aefc83c614b89dafcc
Gonzalo <50145461+gonzaloverdia@users.noreply.github.com>
2021-09-11 18:50:39 -0300

    Add files via upload

---
+++ the_end_of_an_era.js
@@ -0,0 +1,21 @@
+function sayHelloWorld(){
+
+console.log("The end of an era begins with a hello world");
+
+alert("Hello world");
+
+// Dennie van Tassel:
+console.log("I finally understood what 'upward compatible' means.");
+console.log("It means that we keep all our old mistakes.")
+
+}
+
+sayHelloWorld();
+
+
+
+
+
+
+
+
